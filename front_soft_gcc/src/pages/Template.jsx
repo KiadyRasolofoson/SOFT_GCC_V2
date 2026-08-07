d4d58ab19e90fd35609a039045c9624a114de402
Kiady Rasolofoson <kiadyrasolofoson11@gmail.com>
2026-08-07 09:37:37 +0300

    refactor: replace jQuery sidebar toggles with React state and align notification bell with template styles

--- front_soft_gcc/src/pages/Template.jsx
+++ front_soft_gcc/src/pages/Template.jsx
@@ -1,12 +1,20 @@
-import React, { Children } from 'react';
+import React, { useEffect } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import MenuBar from '../components/MenuBar';
 import Footer from '../components/Footer';
 
 // Template des pages
 function Template({children}) {
+  // sidebar-fixed sur body (requis pour les selecteurs CSS .sidebar-fixed.sidebar-icon-only .main-panel)
+  useEffect(() => {
+    document.body.classList.add('sidebar-fixed');
+    return () => {
+      document.body.classList.remove('sidebar-fixed');
+    };
+  }, []);
+
   return (
-    <div className='container-scroller sidebar-fixed'>
+    <div className='container-scroller'>
       <NavigationBar />
       <div className="container-fluid page-body-wrapper">
         <MenuBar />
